47e9a702a8778cce254ed450376ce4b3e2f552e7
Antonio Velazquez <antoniovs1029@gmail.com>
2018-12-04 03:00:11 -0600

    Mas cambios

--- myutils.py
+++ myutils.py
@@ -1,3 +1,8 @@
+"""
+Funciones auxiliares varias para apoyarme en el
+desarrollo del proyecto.
+"""
+
 import random
 import matplotlib.pyplot as plt
 from itertools import zip_longest
@@ -7,6 +12,11 @@ from itertools import zip_longest
 #########################################################
 
 def intersect_most_common(frecs, n = 100):
+    """
+    :param frecs: lista de 3 objetos Counter()
+    :param n: entero
+    :return set intersección de las palabras más comunes de los 3 contadores
+    """
     sets = []
     for i in range(3):
         s = set()
@@ -17,6 +27,11 @@ def intersect_most_common(frecs, n = 100):
     return sets[0] & sets[1] & sets[2]
 
 def disjoin_frecs(frecs, min_frec = 0):
+    """
+    :param frecs: lista de 3 objetos Counter()
+    :param min_frec: entero
+    :return lista de sets de las palabras más frecuentes, que tienen mayor frecuencia a min_frec y que solo son frecuentes en uno de los géneros de películas
+    """
     sets = []
     for i in range(3):
         s = set()
@@ -27,9 +42,14 @@ def disjoin_frecs(frecs, min_frec = 0):
 
     s = (sets[0] | sets[1] | sets[2]) - ((sets[0] & sets[1]) | (sets[0] & sets[2]) | (sets[1] & sets[2]))
 
-    return s
+    return [s & sets[0], s & sets[1], s & sets[2]]
 
 def set2wordfrecs(s, frecs):
+    """
+    :param s: conjunto de palabras
+    :param frecs: lista de objetos Counter
+    :returns una lista de los elementos de s, ordenada y acompañada por sus frecuencias conjuntas en los contadores de frecs
+    """
     res = []
     for elem in s:
         v = 0
@@ -39,14 +59,43 @@ def set2wordfrecs(s, frecs):
 
     return sorted(res, key = lambda tup: tup[1], reverse = True)
 
+def comparar_generos(spa, eng, fre, frecs_spa, frecs_eng, frecs_fre):
+    """
+    :param spa: palabra en español
+    :param eng: palabra en inglés
+    :param fre: palabra en francés
+
+    :param frecs_spa: Counter de palabras en español
+    :param frecs_eng: en inglés
+    :param frecs_fre: en francés
+
+    Imprime de manera organizada las frecuencias de las palabras
+    indicadas en cada lengua y género.
+
+    :returns: Nada
+    """
+    generos = ["accion", "romance", "horror"]
+    for i in range(3):
+        s = frecs_spa[i][spa]
+        e = frecs_eng[i][eng]
+        f = frecs_fre[i][fre]
+
+        print("Genero: {0}, Spa: {1}, Eng: {2}, Fre:{3}".format(generos[i], s, e, f))
+
 #########################################################
 ################### Manipulación de Corpus
 #########################################################
 
 def split_list(l, chunk_size, overlap_size = 0):
     """
-    chunk_size debe ser mayor a 0
+    :param l: lista a segmentar
+    :param chunk_size: tamaño máximo de los segmentos
+    :param overlap_size: overlape entre los segmentos
 
+    :returns lista de listas segmentadas
+
+    Ojo: chunk_size debe ser mayor a 0 y overlap_size
+    debe ser menor a chunk_size
     """
     splitted = []
     imin = 0
@@ -60,6 +109,13 @@ def split_list(l, chunk_size, overlap_size = 0):
     return splitted
 
 def filter_stopwords(l, stopwords):
+    """
+    :param l: lista de palabras
+    :param stopwords: lista de palabras de paro
+
+    :returns lista de palabras que no incluyan a las
+    palabras de paro
+    """
     if stopwords is None:
         return l
 
@@ -70,6 +126,13 @@ def filter_stopwords(l, stopwords):
     return res
 
 def texts2lists(txts, stopwords):
+    """
+    :param txts: lista de strings
+    :param stopwords: lista de palabras de paro
+    
+    :returns lista de listas de palabras, que no
+    incluyan a las palabras de paro
+    """
     lists = []
     for txt in txts:
         txt = txt.strip().split(" ")
@@ -78,6 +141,13 @@ def texts2lists(txts, stopwords):
     return lists
 
 def split_lists(lists, chunk_size, overlap_size = 0):
+    """
+    :param lists: lista de listas a segmentar
+    :param chunk_size: ver "split_list()"
+    :param overlap_size: ver "split_list()"
+
+    :returns lista de listas segmentadas
+    """
     splitted = []
     for l in lists:
         l = split_list(l, chunk_size, overlap_size)
@@ -85,9 +155,24 @@ def split_lists(lists, chunk_size, overlap_size = 0):
     return splitted
 
 def lists2bow(docs, dictionary):
+    """
+    :param docs: lista de listas de palabras
+    :param dictionary: Dictionary de Gensim que asocia
+    un ID a cada palabra.
+
+    :returns lista de vectores dispersos
+    """
     return [dictionary.doc2bow(doc) for doc in docs]
 
 def split_training_txts(txts, training_ratio = .7):
+    """
+    :param txts: lista de strings
+    :training_ratio: porcentaje (del 0 al 1) de qué tan grande
+    debería ser el training set con respecto al test set
+
+    :returns training set y testing set separados y sus
+    respectivas etiquetas
+    """
     training_txts = []
     test_txts = []
 
@@ -108,10 +193,21 @@ def split_training_txts(txts, training_ratio = .7):
     return training_txts, training_labels, test_txts, test_labels
 
 def merge_lists(lists):
+    """
+    :param lists: tupla de listas
+    :returns una lista que combine el contenido de las listas
+    lists, intercalando un elemento de cada lista en cada paso
+    """
     return [v for v in sum(zip_longest(*lists), ()) if v is not None]
     
 
 def merge_multilingual(lists_esp, lists_eng, lists_fre):
+    """
+    Dadas las listas de textos en las 3 lenguas
+    regresa una lista con un solo texto por cada película
+    donde las palabras de las 3 lenguas se intercalan uno
+    a uno.
+    """
     lists_multi = []
     for i in range(len(lists_esp)):
         l = merge_lists((lists_esp[i], lists_eng[i], lists_fre[i]))
